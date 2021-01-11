cfb8093c7f183a6f24e74356379e8d2540766341
James <james.hansung.park@gmail.com>
2021-01-11 18:15:45 -0500

    added sample state shape example

--- 0. Planning/state_shapes.js
+++ 0. Planning/state_shapes.js
@@ -1,4 +1,5 @@
-{
+// delete 'const a ='
+const a = {
     entities: {
         users: {
             1: {
@@ -23,7 +24,8 @@
         // if you need a popup form that covers up the entire page (sign up forms)
         modal: true/false,
         // if you have any videos or music playing
-        currentMedia: "",
+        currentMedia: 2,
+        currentTime: 201
     },
 
     errors: {
