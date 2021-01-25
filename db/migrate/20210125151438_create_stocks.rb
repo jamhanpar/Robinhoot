151eba8828a8e2e06b5bec9109bc5e398c5e2f74
James <james.hansung.park@gmail.com>
2021-01-25 10:18:12 -0500

    created stocks table

---
+++ db/migrate/20210125151438_create_stocks.rb
@@ -0,0 +1,13 @@
+class CreateStocks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :stocks do |t|
+      t.string :name, null: false
+      t.string :ticker, null: false
+
+      t.timestamps
+    end
+
+    add_index :stocks, :name, unique: true
+    add_index :stocks, :ticker, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_220004) do
+ActiveRecord::Schema.define(version: 2021_01_25_151438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "ticker", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_stocks_on_name", unique: true
+    t.index ["ticker"], name: "index_stocks_on_ticker", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
