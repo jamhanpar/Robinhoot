80064b8075770657c9d370a8d7d7d4ea9eb6f505
James <james.hansung.park@gmail.com>
2021-01-28 22:06:38 -0500

    list of stocks for watchlist is not pulling correctly

--- frontend/components/dashboard/watchlist/watchlist_container.js
+++ frontend/components/dashboard/watchlist/watchlist_container.js
@@ -1,10 +1,12 @@
 import { connect } from 'react-redux';
 import WatchlistIndex from './watchlist_index';
-import { fetchPrices } from '../../../actions/price_actions';
+import { fetchBatchPrices } from '../../../actions/price_actions';
 import { fetchUser } from '../../../actions/user_actions';
+import { fetchStocks } from '../../../actions/stock_actions';
 
 const mStP = (state, ownProps) => {
     return {
+    stocks: state.entities.stocks,
     stocksOwned: state.entities.users.stock_ids,
     data: state.entities.prices,
     userId: state.session.currentUser.id
@@ -12,8 +14,9 @@ const mStP = (state, ownProps) => {
 
 const mDtP = dispatch => {
     return {
+    fetchStocks: () => dispatch(fetchStocks()),
     getOwnedStocks: userId => dispatch(fetchUser(userId)),
-    iexFetchData: (symbol, range, interval, apiKey) => dispatch(fetchPrices(symbol, range, interval, apiKey))
+    iexFetchData: (symbols, range, interval, apiKey) => dispatch(fetchBatchPrices(symbols, range, interval, apiKey))
 }};
 
 export default connect(mStP, mDtP)(WatchlistIndex);
--- frontend/reducers/entities/stocks_reducer.js
+++ frontend/reducers/entities/stocks_reducer.js
@@ -1,7 +1,6 @@
 import { RECEIVE_STOCKS, RECEIVE_STOCK } from '../../actions/stock_actions';
-import usersReducer from './users_reducer';
 
-const stocksReducer = (state = {}, action) => {
+const stocksReducer = (state = [], action) => {
     Object.freeze(state);
     let newState = Object.assign({}, state);
 
