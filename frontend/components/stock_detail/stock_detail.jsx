0c30d9b788c2371b199d611eb47523143a89abca
James <james.hansung.park@gmail.com>
2021-02-24 23:54:24 -0500

    updated full disclosure disclaimer css

--- frontend/components/stock_detail/stock_detail.jsx
+++ frontend/components/stock_detail/stock_detail.jsx
@@ -19,89 +19,107 @@ class StockDetail extends React.Component {
 
     render() {
         return (
-            <div>
-                <MainNavContainer />
+          <div>
+            <MainNavContainer />
 
-                <div className="dashboard-container">
-                    <div className="dashboard-content-container">
-                        <div className="portfolio-and-relevant-info-container">
-                            <div className="spacing">
-                                <h1 className="stock-name">{this.state.symbol}</h1>
-                                <StockGraphContainer />
-                            </div>
-                            <div className="company-info-container">
-                                <h1 className="company-about">About</h1>
-                                <div className="company-description">{this.props.companyInfo.description}</div>
-                                <div className="company-info-cards-list">
-                                    <div className="company-info-card">
-                                        <div className="company-title">CEO</div>
-                                        <div className="company-info">{this.props.companyInfo.CEO}</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">Employees</div>
-                                        <div className="company-info">{this.props.companyInfo.employees}</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">Headquarters</div>
-                                        <div className="company-info">{this.props.companyInfo.city}, {this.props.companyInfo.state}</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">Founded</div>
-                                        <div className="company-info">1976</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">Market Cap</div>
-                                        <div className="company-info">Market Cap</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">Price-Earnings Ratio</div>
-                                        <div className="company-info">Price-Earnings Ratio</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">Dividend Yield</div>
-                                        <div className="company-info">Dividend Yield</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">Average Volume</div>
-                                        <div className="company-info">Average Volume</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">High Today</div>
-                                        <div className="company-info">High Today</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">Low Today</div>
-                                        <div className="company-info">Low Today</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">Open Price</div>
-                                        <div className="company-info">Open Price</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">Volume</div>
-                                        <div className="company-info">Volume</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">52 Week High</div>
-                                        <div className="company-info">52 Week High</div>
-                                    </div>
-                                    <div className="company-info-card">
-                                        <div className="company-title">52 Week Low</div>
-                                        <div className="company-info">52 Week Low</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <NewsContainer />
-                            <div className="privacy-policy-container">
-                                <h1 className="privacy-policy-disclaimer">For more information, see our <span className="privacy-policy-link">Privacy Policy</span>.</h1>
-                            </div>
-                        </div>
-            
-                        <TransactionFormContainer />
+            <div className="dashboard-container">
+              <div className="dashboard-content-container">
+                <div className="portfolio-and-relevant-info-container">
+                  <div className="spacing">
+                    <h1 className="stock-name">{this.state.symbol}</h1>
+                    <StockGraphContainer />
+                  </div>
+                  <div className="company-info-container">
+                    <h1 className="company-about">About</h1>
+                    <div className="company-description">
+                      {this.props.companyInfo.description}
                     </div>
+                    <div className="company-info-cards-list">
+                      <div className="company-info-card">
+                        <div className="company-title">CEO</div>
+                        <div className="company-info">
+                          {this.props.companyInfo.CEO}
+                        </div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">Employees</div>
+                        <div className="company-info">
+                          {this.props.companyInfo.employees}
+                        </div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">Headquarters</div>
+                        <div className="company-info">
+                          {this.props.companyInfo.city},{" "}
+                          {this.props.companyInfo.state}
+                        </div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">Founded</div>
+                        <div className="company-info">1976</div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">Market Cap</div>
+                        <div className="company-info">Market Cap</div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">
+                          Price-Earnings Ratio
+                        </div>
+                        <div className="company-info">Price-Earnings Ratio</div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">Dividend Yield</div>
+                        <div className="company-info">Dividend Yield</div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">Average Volume</div>
+                        <div className="company-info">Average Volume</div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">High Today</div>
+                        <div className="company-info">High Today</div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">Low Today</div>
+                        <div className="company-info">Low Today</div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">Open Price</div>
+                        <div className="company-info">Open Price</div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">Volume</div>
+                        <div className="company-info">Volume</div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">52 Week High</div>
+                        <div className="company-info">52 Week High</div>
+                      </div>
+                      <div className="company-info-card">
+                        <div className="company-title">52 Week Low</div>
+                        <div className="company-info">52 Week Low</div>
+                      </div>
+                    </div>
+                  </div>
+                  <NewsContainer />
+                  <div className="privacy-policy-container">
+                    <h1 className="full-disclosure-disclaimer">
+                      All investments involve risks, including the loss of
+                      principal. Securities trading offered through Robinhoot
+                      Financial LLC, a registered broker-dealer and Member SIPC.{" "}
+                      <span className="full-disclosure-link">
+                        Full Disclosure
+                      </span>
+                    </h1>
+                  </div>
                 </div>
+
+                <TransactionFormContainer />
+              </div>
             </div>
-        )
+          </div>
+        );
     }
 }
 
