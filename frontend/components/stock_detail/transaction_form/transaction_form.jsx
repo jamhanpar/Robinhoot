740e5e337076e81a753560f7985a62ae787ea004
James <james.hansung.park@gmail.com>
2021-02-24 23:13:40 -0500

    updated minor css and added transaction toggle

--- frontend/components/stock_detail/transaction_form/transaction_form.jsx
+++ frontend/components/stock_detail/transaction_form/transaction_form.jsx
@@ -3,18 +3,41 @@ import React from 'react';
 class TransactionForm extends React.Component {
     constructor(props) {
         super(props)
+
+        this.state = { status: 'buy' }
+        this.buySelected = 'highlight-selected'
+        this.sellSelected = ''
+    }
+
+    handleClick(field, option) {     
+        this.setState({ [field]: option })        
     }
 
     render() {
+        let costOrCredit = 'cost'
+        let buyingPowerOrOwnedShares = 'Buying Power Available';
+
+        if (this.state.status === 'buy') {
+            costOrCredit = 'cost';
+            buyingPowerOrOwnedShares = 'Buying Power Available';
+            this.buySelected = 'highlight-selected';
+            this.sellSelected = '';
+        } else {
+            costOrCredit = 'credit';
+            buyingPowerOrOwnedShares = 'Shares Available - Sell All';
+            this.buySelected = '';
+            this.sellSelected = 'highlight-selected';
+        }
+
         return (
             <div className="transaction-form-container">
                 <div className="transaction-form">
                     <div className="buy-sell-options-container">
-                        <div className="border-bottom">
-                            <h1 className="buy-option">Buy AAPL</h1>
+                        <div className={`${this.buySelected}`}>
+                            <h1 className="buy-option" onClick={() => this.handleClick('status', 'buy')}>Buy AAPL</h1>
                         </div>
-                        <div className="border-bottom-red">
-                            <h1 className="sell-option">Sell AAPL</h1>
+                        <div className={`sell-option-container ${this.sellSelected}`}>
+                            <h1 className="sell-option" onClick={() => this.handleClick('status', 'sell')}>Sell AAPL</h1>
                         </div>
                     </div>
                     <div className="transaction-inputs-container">
@@ -29,12 +52,11 @@ class TransactionForm extends React.Component {
                         <div className="form-elements market-price-container">
                             <div className="market-price-title-container">
                                 <h1 className="transaction-title-green market-price">Market Price</h1>
-                                <img className="faq-icon" src={window.faqIcon} />
                             </div>
                             <p className="transaction-title-bold">$136.60</p>
                         </div>
                         <div className="form-elements estimated-cost-container">
-                            <h1 className="transaction-title-bold estimated-cost">Estimated Cost</h1>
+                            <h1 className="transaction-title-bold estimated-cost">Estimated {costOrCredit}</h1>
                             <p className="transaction-title-bold">$0.00</p>
                         </div>
                         <div className="review-btn-container">
@@ -43,7 +65,7 @@ class TransactionForm extends React.Component {
                     </div>
                     {/* <div className="border-top"> */}
                     <div className="buying-power-show-container">
-                        <p className="transaction-title-green buying-power-available">$100.00 Buying Power Available</p>
+                        <p className="transaction-title-green buying-power-available">$100.00 {buyingPowerOrOwnedShares}</p>
                     </div>
                     {/* </div> */}
                 </div>
