d3cf9b5fe7d0e074f84abbc4619d80281071faf5
James <james.hansung.park@gmail.com>
2021-03-03 20:29:38 -0500

    updated css for watchlist

--- frontend/components/dashboard/watchlist/watchlist_index.jsx
+++ frontend/components/dashboard/watchlist/watchlist_index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import WatchlistIndexItem from './watchlist_index_item';
+import { FaBolt } from "react-icons/fa";
 
 export default class WatchlistIndex extends React.Component {
     constructor(props) {
@@ -24,43 +25,47 @@ export default class WatchlistIndex extends React.Component {
         this.props.iexFetchData('AAPL', this.state.range, this.state.interval, window.iexcloudAPIKey)
     }
 
-    // renderWatchlists() {
-        
-    // }
-
-    render() {
+    renderWatchlists() {
         const {watchlists, currentUserID} = this.props;
         let currentUserWatchlist = Object.values(watchlists).filter(watchlist => {return watchlist.user_id === parseInt(currentUserID)})
 
-        debugger
-        if (!watchlists) return null;
+        return (
+            currentUserWatchlist.map((watchlist, i) => (
+                <div className="list-container">
+                    <div className="list-title-container">
+                        <FaBolt className="lightning-icon"/>
+                        <div className="list-title">{watchlist.watchlist_name}</div>
+                    </div>
+                    {
+                        watchlist.watched_stocks.map((stock, i) => (
+                            <Link to={`/stocks/${stock.stock_symbol}`}>
+                                <WatchlistIndexItem
+                                    key={`${stock.stock_symbol}-${i}`}
+                                    symbol={stock.stock_symbol}
+                                    data={this.props.data}
+                                />
+                            </Link>
+                        ))
+                    }
+                </div>
+            ))
+        )
+    }
 
-        let renderWatchlists = currentUserWatchlist.map((watchlist, i) => (
-            <div>
-              <div>{watchlist.watchlist_name}</div>
-              {watchlist.watched_stocks.map((stock, i) => (
-                <Link to={`/stocks/${stock.stock_symbol}`}>
-                  <WatchlistIndexItem
-                    key={`${stock.stock_symbol}-${i}`}
-                    symbol={stock.stock_symbol}
-                    data={this.props.data}
-                  />
-                </Link>
-              ))}
-            </div>
-          ));
-        
+    render() {
+        if (!this.props.watchlists) return null;
+
         return (
             <div className="watchlist-container">
                 <div className="watchlist-title-container">
                     <h1 className="watchlist-title">Stocks</h1>
                 </div>
-                {/* <Link to={`/stocks/symbol`}><WatchlistIndexItem data={this.props.data}/></Link>
-                <Link to={`/stocks/symbol`}><WatchlistIndexItem data={this.props.data}/></Link> */}
+                <Link to={`/stocks/symbol`}><WatchlistIndexItem data={this.props.data}/></Link>
+                <Link to={`/stocks/symbol`}><WatchlistIndexItem data={this.props.data}/></Link>
                 <div className="watchlist-title-container">
                     <h1 className="watchlist-title">Lists</h1>
                 </div>
-                {renderWatchlists}
+                {this.renderWatchlists()}
             </div>
         )
     }
