f9e080158c17d52d8423f6e197f3e691a68ca3b4
James Park <james.hansung.park@gmail.com>
2021-03-17 16:03:01 -0400

    reformatting news card information

--- frontend/components/dashboard/news/news_card.jsx
+++ frontend/components/dashboard/news/news_card.jsx
@@ -6,9 +6,9 @@ const NewsCard = ({ news }) => {
             <div className="news-card">
                 <div className="news-card-outline">
                     <div className="news-info-list">
-                        <div className="news-card-source">Source: {news.source}</div>
-                        <div className="news-card-headline">Headline: {news.headline}</div>
-                        <div className="news-card-related-symbols">Related: {news.related}</div>
+                        <div className="news-card-source">{news.source}</div>
+                        <div className="news-card-headline">{news.headline}</div>
+                        <div className="news-card-related-symbols">{news.related}</div>
                     </div>
                     <div className="news-image-container">
                         <img className="source-menu-icon" src={window.newsDots} />
