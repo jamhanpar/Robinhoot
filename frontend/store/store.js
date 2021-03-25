2f2a50277b1dba6030b31676b6597a1f976bcc15
James Park <james.hansung.park@gmail.com>
2021-03-25 14:37:35 -0400

    updated redux logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,8 +3,6 @@ import rootReducer from '../reducers/root_reducer';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
-const configureStore = (preloadedState = {}) => (
-    createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger))
-);
+const configureStore = (preloadedState = {}) => (createStore(rootReducer, preloadedState, applyMiddleware(thunk, logger)));
 
 export default configureStore;
