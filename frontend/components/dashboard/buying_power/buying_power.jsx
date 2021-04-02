02556ca14fc91f286d26631dea90eb69ef616f8f
James Park <james.hansung.park@gmail.com>
2021-04-01 22:40:38 -0400

    💻: refactored code

--- frontend/components/dashboard/buying_power/buying_power.jsx
+++ frontend/components/dashboard/buying_power/buying_power.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class BuyingPower extends React.Component {
-    constructor(props) {
-        super(props)
-    }
+    // constructor(props) {
+    //     super(props)
+    // }
 
     render() {
         return (
