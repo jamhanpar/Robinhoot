d76847591466449f239ebcc9f261f971066d689b
James Park <james.hansung.park@gmail.com>
2021-04-01 22:40:12 -0400

    💻: refactored code

--- frontend/components/dashboard/buying_power/buying_power_container.js
+++ frontend/components/dashboard/buying_power/buying_power_container.js
@@ -1,12 +1,8 @@
 import { connect } from 'react-redux';
 import BuyingPower from './buying_power';
 
-const mStP = (state, ownProps) => ({
+const mStP = (state, ownProps) => ({});
 
-});
-
-const mDtP = dispatch => ({
-
-});
+const mDtP = dispatch => ({});
 
 export default connect(mStP, mDtP)(BuyingPower)
